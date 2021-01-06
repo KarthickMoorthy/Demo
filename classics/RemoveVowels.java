6de80f70456675e5687445f933113ffeb27d941d
Karthick <karthickmorrthy91@gmail.com>
2021-01-06 11:36:48 -0600

    Vowel fix

--- classics/RemoveVowels.java
+++ classics/RemoveVowels.java
@@ -7,15 +7,15 @@ public class RemoveVowels {
 
     static String remVowel(String str)
     {
-        Character vowels[] = {'a', 'e', 'i', 'o', 'u','A','E','I','O','U'};
+        Character[] vowels = {'a', 'e', 'i', 'o', 'u','A','E','I','O','U'};
 
         List<Character> al = Arrays.asList(vowels);
 
-        StringBuffer sb = new StringBuffer(str);
+        StringBuilder sb = new StringBuilder(str);
 
         for (int i = 0; i < sb.length(); i++) {
 
-            if(al.contains(sb.charAt(i))){
+            if(al.contains(sb.charAt(i)) || Character.isDigit(sb.charAt(i))){
                 sb.replace(i, i+1, "") ;
                 i--;
             }
@@ -27,7 +27,7 @@ public class RemoveVowels {
     // Driver method to test the above function
     public static void main(String[] args)
     {
-        String str = "GeeeksforGeeks - A Computer Science Portal for Geeks";
+        String str = "12 45 3 GeeeksforGeeks - A Computer Science Portal for Geeks";
 
         System.out.println(remVowel(str));
     }
